395a609a966349ecabada4fafe8f20894cc3c88e
Malaikathecoolgirl <malaikathecoolgirl@gmail.com>
2024-09-18 14:10:48 +0200

    Enemy

--- Enemies/enemy.gd
+++ Enemies/enemy.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+
 var speed = 20
 var player_chase = false
 var player = null
@@ -7,17 +8,17 @@ var player = null
 
 func _physics_process(delta):
 	if player_chase and player != null and player.has_method("get_position"):
-		position += (player.position - position).normalized() * speed * delta
+		#position += (player.position - position).normalized() * speed * delta
+		velocity = (player.position - position).normalized() * speed
 
-
+		print(velocity)
 		if player.position.x > position.x:
 			$AnimatedSprite2D.play("Right")
 		elif player.position.x < position.x:
 			$AnimatedSprite2D.play("Left")
-		elif player.position.y < self.position.y and player.position.x > position.x:
-			$AnimatedSprite2D.play("Back")
-		elif player.position.y > self.position.y and player.position.x < position.x:
-			$AnimatedSprite2D.play("Forward")
+			
+		move_and_slide()
+
 
 
 func _on_detection_area_body_entered(body: Node2D) -> void:
